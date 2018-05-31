a359398847d917a0f1b8bd162175a8c3ef1ebd3f
Charly POLY <cpoly55@gmail.com>
2018-05-31 18:48:56 +0200

    fix(packaging): move lib/index to root (.)

---
+++ index.ts
@@ -0,0 +1,2 @@
+export * from './lib/fromIntrospectionQuery';
+// export * from './fromGraphQLSchema';
--- lib/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './fromIntrospectionQuery';
-// export * from './fromGraphQLSchema';
